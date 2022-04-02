fcfeb84292adf28d26ffb35b8726cecf431edca6
Renan Souza <renantrsouza@gmail.com>
2022-04-02 15:44:29 -0400

    use setState and remove old props

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -6,16 +6,16 @@ import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 
 const ListaDeEventos: React.FC<{ 
-  aoAlterarStatus: (id: number) => void, 
+  //aoAlterarStatus: (id: number) => void, 
   //aoDeletarEvento: (id: number) => void, 
-  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoAlterarStatus, aoFiltroAplicado }) => {
+  aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
 
   const eventos = useRecoilValue(listaDeEventosState)
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
       {eventos.map(evento => (
-        <Evento aoAlterarStatus={aoAlterarStatus} evento={evento} key={evento.id} />
+        <Evento evento={evento} key={evento.id} />
       ))}
     </div>
   </section>)
