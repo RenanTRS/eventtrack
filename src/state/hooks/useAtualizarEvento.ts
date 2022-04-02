ba05fafdd0489b4b8a4052cd2593a6953cb7e4e3
Renan Souza <renantrsouza@gmail.com>
2022-04-02 16:21:35 -0400

    create and use hook

---
+++ src/state/hooks/useAtualizarEvento.ts
@@ -0,0 +1,15 @@
+import { listaDeEventosState } from './../atom';
+import { useSetRecoilState } from "recoil"
+import { IEvento } from "../../interfaces/IEvento"
+
+const useAtualizarEvento = () => {
+    const setListaDeEventos = useSetRecoilState<IEvento[]>(listaDeEventosState)
+    return (evento: IEvento) => {
+        return setListaDeEventos(listaAntiga => {
+            const indice = listaAntiga.findIndex(evt => evt.id === evento.id)
+            return [...listaAntiga.slice(0, indice), evento, ...listaAntiga.slice(indice + 1)]
+        })
+    }
+}
+
+export default useAtualizarEvento
