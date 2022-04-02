9999929098ecfecdcfa705f1a71d4d28e1fc026c
Renan Souza <renantrsouza@gmail.com>
2022-04-02 16:51:05 -0400

    create and use hook

--- src/components/ListaDeEventos/index.tsx
+++ src/components/ListaDeEventos/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
-import { useRecoilValue } from 'recoil';
-import { listaDeEventosState } from '../../state/atom';
+import useListaDeEventos from '../../state/hooks/useListaDeEventos';
 import Evento from '../Evento';
 import Filtro from '../Filtro';
 import style from './ListaDeEventos.module.scss';
 
 const ListaDeEventos: React.FC<{ 
-  //aoAlterarStatus: (id: number) => void, 
-  //aoDeletarEvento: (id: number) => void, 
   aoFiltroAplicado: (data: Date | null) => void }> = ({ aoFiltroAplicado }) => {
 
-  const eventos = useRecoilValue(listaDeEventosState)
+  const eventos = useListaDeEventos()
   return (<section>
     <Filtro aoFiltroAplicado={aoFiltroAplicado} />
     <div className={style.Scroll}>
